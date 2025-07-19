82c7d853622b8b6109bfa9a15ec450b9f363cd0a
Martín Rosso <mrosso10@gmail.com>
2025-07-19 15:01:56 -0300

    fixup: log tenant params

--- lib/acts_as_tenant/active_job_extensions.rb
+++ lib/acts_as_tenant/active_job_extensions.rb
@@ -11,11 +11,18 @@ module ActsAsTenant
       tenant_global_id = job_data.delete("current_tenant")
       tenant_unscoped = job_data.delete("tenant_unscoped")
 
+      job_id = job_data["job_id"]
+      job_class = job_data["job_class"]
       if tenant_unscoped
-        Rails.logger.warn "WARN: Running job with ActsAsTenant unscoped. Admin?"
+        Rails.logger.tagged(job_class, job_id).warn "WARN: Running job with ActsAsTenant unscoped. Admin?"
         ActsAsTenant.unscoped = true
       else
-        ActsAsTenant.current_tenant = tenant_global_id ? GlobalID::Locator.locate(tenant_global_id) : nil
+        if tenant_global_id
+          Rails.logger.tagged(job_class, job_id).info "Running job with tenant: #{tenant_global_id}"
+          ActsAsTenant.current_tenant = GlobalID::Locator.locate(tenant_global_id)
+        else
+          ActsAsTenant.current_tenant = nil
+        end
       end
 
       super
