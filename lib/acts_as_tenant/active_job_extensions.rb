d2d7701b994d77be33c2c7a6e531e6cc920e894c
Martín Rosso <mrosso10@gmail.com>
2025-07-19 15:01:56 -0300

    feat: unscoped active jobs

--- lib/acts_as_tenant/active_job_extensions.rb
+++ lib/acts_as_tenant/active_job_extensions.rb
@@ -1,12 +1,23 @@
 module ActsAsTenant
   module ActiveJobExtensions
     def serialize
-      super.merge("current_tenant" => ActsAsTenant.current_tenant&.to_global_id&.to_s)
+      super.merge(
+        "current_tenant" => ActsAsTenant.current_tenant&.to_global_id&.to_s,
+        "tenant_unscoped" => ActsAsTenant.unscoped?
+      )
     end
 
     def deserialize(job_data)
       tenant_global_id = job_data.delete("current_tenant")
-      ActsAsTenant.current_tenant = tenant_global_id ? GlobalID::Locator.locate(tenant_global_id) : nil
+      tenant_unscoped = job_data.delete("tenant_unscoped")
+
+      if tenant_unscoped
+        Rails.logger.warn "WARN: Running job with ActsAsTenant unscoped. Admin?"
+        ActsAsTenant.unscoped = true
+      else
+        ActsAsTenant.current_tenant = tenant_global_id ? GlobalID::Locator.locate(tenant_global_id) : nil
+      end
+
       super
     end
   end
